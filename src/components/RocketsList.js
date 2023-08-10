3c5255c3965d64d4eef1da7f74515c4cb4e1b7bb
akbrsazish <sazishaliakbar@gmail.com>
2023-08-10 16:50:23 +0330

    solove liners error and add reserved, reserve rocket and cancel reserveation

--- src/components/RocketsList.js
+++ src/components/RocketsList.js
@@ -1,8 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import fetchRockets from './actions';
 
 const RocketsList = () => {
+  const [reserved, setReserved] = useState(false);
+  const handleReserveClick = () => {
+    setReserved(true);
+  };
+  const handleCancelReserveClick = () => {
+    setReserved(false);
+  };
+
   const dispatch = useDispatch();
   const rockets = useSelector((state) => state.rockets.rockets);
 
@@ -12,12 +20,18 @@ const RocketsList = () => {
 
   return (
     <div>
-      <h2>Rockets List</h2>
       {rockets.map((rocket) => (
-        <div key={rocket.id}>
-          <h3>{rocket.rocket_name}</h3>
-          <p>{rocket.description}</p>
-          <img src={rocket.flickr_images[0]} alt={rocket.rocket_name} />
+        <div className="rocket-container" key={rocket.id}>
+          <div className="rocket-item-image">
+            <img className="rocket-image" src={rocket.flickr_images[0]} alt={rocket.rocket_name} />
+          </div>
+          <div className="rocket-item">
+            {reserved ? (<span className="reserved" id="reserved"> Reserved </span>) : ''}
+            <span className="rocket-name">{rocket.name}</span>
+            <p className="rocket-description">{rocket.description}</p>
+            {reserved ? (<button type="button" className="cancel-button" id="cancelReserveBtn" onClick={handleCancelReserveClick}> Cancel Reserve </button>) : (
+              <button type="button" className="reserve-button" id="reserveBtn" onClick={handleReserveClick}> Reserve Rocket </button>)}
+          </div>
         </div>
       ))}
     </div>
