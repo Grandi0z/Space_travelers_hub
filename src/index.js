b9466852dfff8943559cebe546c6930b79123da6
Joseph Lwanzo Khausi <josephlkausi@gmail.com>
2023-08-10 15:19:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import './styles/index.css';
 import App from './App';
-import store from './redux/features/store';
+import store from './redux/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 /*eslint-disable*/
